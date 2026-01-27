db8a1d4e3d97c209fa363d0383f08d238536b070
Madden-R <noodlerota@gmail.com>
2026-01-26 21:10:43 -0800

    Add buttons and signals to the main menu
    Added a start and quit button to the main menu. Added signals & print statements to the MainMenu that activate on button presses.

--- MainGame/Menus/Scripts/MainMenu.cs
+++ MainGame/Menus/Scripts/MainMenu.cs
@@ -3,11 +3,36 @@ using System;
 
 public partial class MainMenu : Menu
 {
+	private Button StartButton;
+	private Button QuitButton;
+
+	[Signal]
+	public delegate void StartButtonPressedEventHandler();
+
+	[Signal]
+	public delegate void QuitButtonPressedEventHandler();
 
 	public override void _Ready()
 	{
 		base._Ready();
-		GD.Print("Main Menu successfully loaded in!");
+		
+		StartButton = GetNode<Button>("MarginContainer/VBoxContainer/StartButton");
+		QuitButton = GetNode<Button>("MarginContainer/VBoxContainer/QuitButton");
+
+        StartButton.ButtonDown += () => OnStartButtonPressed();
+        QuitButton.ButtonDown += () => OnQuitButtonPressed();
+	}
+
+	private void OnStartButtonPressed()
+	{
+		EmitSignal(SignalName.StartButtonPressed);
+        GD.Print("Start Button Pressed!");
+	}
+
+	private void OnQuitButtonPressed()
+	{
+		EmitSignal(SignalName.QuitButtonPressed);
+        GD.Print("Quit Button Pressed!");
 	}
 
 }
