aef6b5d91371376cdee8498983c05edc87081cdd
Madden-R <noodlerota@gmail.com>
2026-01-26 20:36:52 -0800

    Change Menu class to be abstract
    Menu class not intended to be used on its own. Instead as a base class for other menus.

--- MainGame/Menus/Scripts/Menu.cs
+++ MainGame/Menus/Scripts/Menu.cs
@@ -2,7 +2,7 @@ using Godot;
 using System;
 
 // This is a class meant to provide functionality for menus. 
-public partial class Menu : Control
+public abstract partial class Menu : Control
 {
 	
 }
