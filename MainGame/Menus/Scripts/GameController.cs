0b0b9240db851da05544018b511062b648059409
Madden-R <noodlerota@gmail.com>
2026-01-26 20:27:20 -0800

    Add GameController and Menu scripts
    Added a GameController script which will be used to handle swapping between different scenes and displaying transitions and splash screens.
    
    Also added a Menu script which defines a Menu class where menus like the main menu & options menu will inherit from it.

---
+++ MainGame/Menus/Scripts/GameController.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class GameController : Node
+{
+	private Menu MainMenu;
+
+	public GameController()
+	{
+		
+	}
+}
---
+++ MainGame/Menus/Scripts/Menu.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+// This is a class meant to provide functionality for menus. 
+public partial class Menu : Control
+{
+    
+}
