d3b19df7d75f77bcded784706d629b140e8c72cd
Madden-R <noodlerota@gmail.com>
2026-01-26 20:54:35 -0800

    Add functionality to GameController to load MainMenu on _Ready
    GameController now instantiates a MainMenu scene on _Ready(). Also Added logic to MainMenu to print a statement upon seccessfuly entering the scene tree.

--- MainGame/Menus/Scripts/GameController.cs
+++ MainGame/Menus/Scripts/GameController.cs
@@ -3,10 +3,23 @@ using System;
 
 public partial class GameController : Node
 {
+
+	public const string MainMenuFilepath = "res://Menus/Scenes/main_menu.tscn";
 	private Menu MainMenu;
 
 	public GameController()
 	{
-		
 	}
+
+	public override void _Ready()
+	{
+		base._Ready();
+		
+		// Instantiate a Main Menu & add it to the scene tree
+		Menu TempMainMenu = (Menu)ResourceLoader.Load<PackedScene>(MainMenuFilepath).Instantiate();
+		MainMenu = TempMainMenu;
+		AddChild(MainMenu);
+
+	}
+
 }
--- MainGame/Menus/Scripts/MainMenu.cs
+++ MainGame/Menus/Scripts/MainMenu.cs
@@ -3,4 +3,11 @@ using System;
 
 public partial class MainMenu : Menu
 {
+
+	public override void _Ready()
+	{
+		base._Ready();
+		GD.Print("Main Menu successfully loaded in!");
+	}
+
 }
