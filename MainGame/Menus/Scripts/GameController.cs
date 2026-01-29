3f45234e5e11b045a4889aa6a596f31ede2c927f
Madden-R <noodlerota@gmail.com>
2026-01-28 20:51:29 -0800

    Add CurrentOpenedMenu and OpenMenu
    CurrentOpenedMenu stores the current On-Screen Menu. OpenMenu opens a menu by removing the CurrentOpenedMenu if there is one & setting it to the new one.

--- MainGame/Menus/Scripts/GameController.cs
+++ MainGame/Menus/Scripts/GameController.cs
@@ -7,6 +7,7 @@ public partial class GameController : Node
 	public const string MainMenuFilepath = "res://Menus/Scenes/main_menu.tscn";
 
 	private MainMenu CurrentMainMenu; // currently loaded main menu
+	private Menu CurrentOpenedMenu; // menu currently on screen
 
 	public GameController()
 	{
@@ -19,8 +20,21 @@ public partial class GameController : Node
 		
 		// Instantiate a Main Menu & add it to the scene tree
 		CurrentMainMenu = LoadMainMenu();
-		AddChild(CurrentMainMenu);
+		OpenMenu(CurrentMainMenu);
 
+	}
+
+	// opens the menu: Removes the current Menu from the scene tree.
+	// Adds the menu M to the scene tree & makes it the current Menu.
+	private void OpenMenu(Menu M)
+	{
+		if (CurrentOpenedMenu is not null)
+		{
+			RemoveChild(CurrentOpenedMenu);
+		}
+
+		CurrentOpenedMenu = M;
+		AddChild(CurrentOpenedMenu);
 	}
 
 	// Load and return a new MainMenu node. Initialize & connect its signals.
