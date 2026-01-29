db61de1c04784593b04d47f71936d736f3bcfae5
Madden-R <noodlerota@gmail.com>
2026-01-28 20:43:27 -0800

    Add signals for OptionsButton

--- MainGame/Menus/Scripts/MainMenu.cs
+++ MainGame/Menus/Scripts/MainMenu.cs
@@ -13,15 +13,20 @@ public partial class MainMenu : Menu
 	[Signal]
 	public delegate void QuitButtonPressedEventHandler();
 
+	[Signal]
+	public delegate void OptionsButtonPressedEventHandler();
+
 	public override void _Ready()
 	{
 		base._Ready();
 		
+		// Initialize buttons and connect signals
 		StartButton = GetNode<Button>("MarginContainer/VBoxContainer/StartButton");
 		QuitButton = GetNode<Button>("MarginContainer/VBoxContainer/QuitButton");
 		OptionsButton = GetNode<Button>("MarginContainer/VBoxContainer/OptionsButton");
 
 		StartButton.ButtonDown += () => OnStartButtonPressed();
+		OptionsButton.ButtonDown += () => OnOptionsButtonPressed();
 		QuitButton.ButtonDown += () => OnQuitButtonPressed();
 	}
 
@@ -31,10 +36,15 @@ public partial class MainMenu : Menu
 		EmitSignal(SignalName.StartButtonPressed);
 	}
 
+	private void OnOptionsButtonPressed()
+	{
+		GD.Print("Options Button Pressed!");
+		EmitSignal(SignalName.OptionsButtonPressed);
+	}
+
 	private void OnQuitButtonPressed()
 	{
 		GD.Print("Quit Button Pressed!");
 		EmitSignal(SignalName.QuitButtonPressed);
 	}
-
 }
