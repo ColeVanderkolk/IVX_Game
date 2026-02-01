b96c6d7c3bacd20bed60f424ba4fa1b500f389fe
Phineythedog <parentp2@wwu.edu>
2026-02-01 13:18:21 -0800

    Implemented movement functions to horde

--- MainGame/Units/horde.gd
+++ MainGame/Units/horde.gd
@@ -5,22 +5,32 @@ var avgSpeed : float
 
 @export var radius : float = 2.0;
 
+var moving = false
+var targetX = null
+var targetZ = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	print(moving)
+	if moving:
+		move(delta)
 
-func addUnit():
+func addUnit(health = 1, damage = 1, speed = 1):
 	var newUnit = load("res://Units/unit.tscn").instantiate()
+	newUnit.health = health
+	newUnit.damage = damage
+	newUnit.speed = speed
 	units.append(newUnit)
 	add_child(newUnit)
 	
 	repositionUnits()
 	
 	setAvgSpeed()
+
 
 func setPosition(unit : Node, index : int):
 	if index > 1:
@@ -54,3 +64,23 @@ func mitosis(numUnits : int):
 	
 	get_parent().add_child(newHorde)
 	return newHorde
+
+func startMoving(xCoord : float, zCoord : float):
+	moving = true
+	targetX = xCoord
+	targetZ = zCoord
+
+func move(delta : float):
+	var dirX = getDirection(position.x, targetX)
+	var dirZ = getDirection(position.z, targetZ)
+	position.x += avgSpeed * delta * dirX
+	position.z += avgSpeed * delta * dirZ
+	
+	if position.x == targetX:
+		moving = false
+
+func getDirection(coord1, coord2):
+	if coord1 > coord2:
+		return -1
+	else:
+		return 1
--- MainGame/Units/horde_example_environment.gd
+++ MainGame/Units/horde_example_environment.gd
@@ -3,24 +3,15 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#$Horde.position.x -= 5
-	$Horde.addUnit()
-	$Horde.addUnit()
-	$Horde.addUnit()
-	$Horde.addUnit()
-	$Horde.addUnit()
-	$Horde.addUnit()
-	$Horde.addUnit()
-	$Horde.addUnit()
-	$Horde.addUnit()
-	$Horde.addUnit()
-	$Horde.addUnit()
-	$Horde.addUnit()
-	$Horde.addUnit()
-	#var new = $Horde.mitosis(2)
-	#new.position. x += 5
-
+	$Horde.addUnit(1, 1, 1)
+	
+	$Horde.startMoving(200, 200)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_timer_timeout() -> void:
+	$Timer.start()
+	$Horde.addUnit()
