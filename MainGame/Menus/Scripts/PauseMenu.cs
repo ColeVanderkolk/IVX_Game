7fc7acbae793c434d546f16a574469063b22fa17
Madden-R <noodlerota@gmail.com>
2026-02-01 21:03:16 -0800

    Add functionality to 'Return to Game' button in Pause Menu

--- MainGame/Menus/Scripts/PauseMenu.cs
+++ MainGame/Menus/Scripts/PauseMenu.cs
@@ -3,4 +3,50 @@ using System;
 
 public partial class PauseMenu : CanvasLayer
 {
+
+	private bool IsActive;
+
+	private Button ReturnButton;
+	private Button QuitButton;
+
+	public override void _Ready()
+	{
+		base._Ready();
+
+		Visible = false;
+		IsActive = false;
+
+		ReturnButton = GetNode<Button>("Control/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/ReturnButton");
+		QuitButton = GetNode<Button>("Control/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/QuitButton");
+
+		ReturnButton.ButtonDown += () => ReturnButtonPressed();
+		QuitButton.ButtonDown += () => QuitButtonPressed();
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		base._Input(@event);
+		if (@event.IsActionPressed("Escape") && IsInsideTree())
+		{
+			SetActive(! IsActive);
+		}
+	}
+
+	private void SetActive(bool active)
+	{
+		IsActive = active;
+		Visible = active;
+	}
+
+	private void ReturnButtonPressed()
+	{
+		GD.Print("Return Button Pressed!");
+		SetActive(false);
+	}
+
+	private void QuitButtonPressed()
+	{
+		
+	}
+
 }
