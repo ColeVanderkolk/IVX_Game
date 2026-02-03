f4241730c8a729b00b1e2976fd4f4d813933ce8c
shawjus <jedijetjustin@gmail.com>
2026-02-03 15:54:26 -0800

    add mine manager for contralized timer

--- MainGame/Buildings/Scripts/buildings_colliderscript.cs
+++ MainGame/Buildings/Scripts/buildings_colliderscript.cs
@@ -5,18 +5,18 @@ public partial class buildings_colliderscript : StaticBody3D
 {
 
 	[Signal]
-	public delegate void BuildingClickedEventHandler();
+	public delegate void BuildingClickedEventHandler(Vector3 mousePos);
 
 
 	// Emits a signal to indicate this building has been mouse clicked for future UI purposes
-	private void OnMouseClick(Node camera, InputEvent input, Vector3 pos, Vector3 normal, int shape_idx)
+	private void OnMouseEvent(Node camera, InputEvent input, Vector3 pos, Vector3 normal, int shape_idx)
 	{
 		if (input is InputEventMouseButton mouseButton)
 		{
 			if(mouseButton.Pressed == true && mouseButton.ButtonIndex == MouseButton.Left)
 			{
 				GD.Print("Clicked");
-				EmitSignal(SignalName.BuildingClicked);
+				EmitSignal(SignalName.BuildingClicked, pos);
 			}
 		}
 	}
--- MainGame/Buildings/Scripts/mine_buildingscript.cs
+++ MainGame/Buildings/Scripts/mine_buildingscript.cs
@@ -7,7 +7,8 @@ public partial class mine_buildingscript : Node3D
 	private Timer mineTimer;
 
 	// Coins (?) generated per second
-	private const int COIN_RATE = 5;
+	[Export]
+	private int COIN_RATE = 5;
 	
 	// Whether this mine can function
 	private bool Assimilated = false;
@@ -21,28 +22,31 @@ public partial class mine_buildingscript : Node3D
 
 	public override void _Ready()
 	{
-		mineTimer = GetNode<Timer>("Timer");
+		mineTimer = GetNode<Timer>("../Timer");
 		OldMesh = GetNode<Node3D>("DefunctMesh");
 		NewMesh = GetNode<Node3D>("AssimMesh");
 	} 
 
+	// Add whatever currency this mine generates to manager
+	// signalled every second while assimillated
 	private void OnMineTimeout()
 	{
-		if(Assimilated)
-		{
-			mineTimer.Start();
-			GD.Print("+5");
-			// currencymanager_script.add_currency((int)CURRENCIES.COIN, COIN_RATE);
-		}
+		GD.Print("+5");
+		// currencymanager_script.add_currency((int)CURRENCIES.COIN, COIN_RATE);
 	}
 
 	// Should be signalled once the mine has been assimillated
-	private void OnAssimilate()
+	// toggles assimillated model and subscribes to mineTimer to produce
+	private void OnAssimilate(Vector3 mousePos)
 	{
-		Assimilated = true;
-		NewMesh.Visible = !NewMesh.Visible;
-		OldMesh.Visible = !OldMesh.Visible;
-		mineTimer.Start();
+		if(!Assimilated)
+		{
+			Assimilated = true;
+			NewMesh.Visible = true;
+			OldMesh.Visible = false;
+			mineTimer.Timeout += OnMineTimeout;
+		}
+
 	}
 
 }
