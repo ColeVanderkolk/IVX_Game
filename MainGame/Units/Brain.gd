99ef402a37403e926daa49b3043ee1cd3466fae8
NoicePlayer <ianftw3@gmail.com>
2026-02-04 22:48:24 -0800

    when brain dies, emit gameover

--- MainGame/Units/Brain.gd
+++ MainGame/Units/Brain.gd
@@ -9,3 +9,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+signal gameOver()
+
+func _unit_death():
+	gameOver.emit()
